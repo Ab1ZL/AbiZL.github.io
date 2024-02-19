885c8012d15c3e86d43b53c5f040ba1615547324
Ab1ZL <160424708+Ab1ZL@users.noreply.github.com>
2024-02-19 13:17:24 -0600

    Create build.sh

---
+++ Build/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Imprime un mensaje para indicar que el proceso de build ha comenzado
+echo "Iniciando proceso de build..."
+
+# Ejecuta los comandos necesarios para compilar y construir tu proyecto
+# Aquí incluye todos los comandos que necesites para preparar tu proyecto para producción
+# Por ejemplo, podrías ejecutar comandos para instalar dependencias, compilar archivos fuente, minificar recursos, etc.
+
+# Ejemplo:
+# Instalación de dependencias con npm
+npm install
+
+# Compilación de archivos fuente (por ejemplo, usando Webpack)
+npm run build
+
+# Imprime un mensaje para indicar que el proceso de build ha finalizado
+echo "Proceso de build finalizado."
